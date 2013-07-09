207633e7067755bdf9ead796cb5378bd5d85e037
Adam Murray <adam.john.campbell.murray@gmail.com>
2013-07-09 17:55:56 +0100

    'further_updates'

--- main/mastersfirststep.pas
+++ main/mastersfirststep.pas
@@ -19,14 +19,16 @@ uses bmp;
 
 
 (*---------- Type declarations ----------*)
-{type
-   ;}
+type
+   pint = ^integer;
 
 
 (*---------- Variable declarations ----------*)
 var
-   leftimagein, rightimagein	       : string;
-   leftimage, rightimage, productimage : pimage;
+   currentDate, currentTime, productImageLocation : string;
+   leftImageLocation, rightImageLocation	  : string;
+   leftImage, rightImage, productImage		  : pimage;
+   leftImageTemp, rightImageTemp		  : pimage;
 
 
 (*---------- Functions ----------*)
@@ -35,17 +37,17 @@ var
  *  Function to load in both images.
  *  Returns boolean -- true if images read in properly, false otherwise
  *)
-function loadimages(limagein, rimagein : string):boolean;
+function loadImages(limagein, rimagein : string):boolean;
 begin
-   {take a string as source and a pimage (from bmp) to load image}
-   if loadbmpfile(leftimagein, leftimage)
-      and loadbmpfile(rightimagein, rightimage) then
+   {take a string as source and a pimage to load image}
+   if loadbmpfile(leftImageLocation, leftImage)
+      and loadbmpfile(rightImageLocation, rightImage) then
    begin
-      loadimages := true;
+      loadImages := true;
    end
    else
    begin
-      loadimages := false;
+      loadImages := false;
    end;
 end; { loadimages }
 
@@ -53,41 +55,73 @@ end; { loadimages }
 (*---------- Procedures ----------*)
 
 (*
+ *  Procedure to get user input at the
+ *  beginning of the program
+ *)
+procedure userInput;
+begin
+   writeln('// First step for masters algorithm');
+   writeln('// Reads in two images and applies a correlation');
+   writeln;
+   write('Enter the current date (DD-MM-YY): ');
+   readln(currentDate);
+   write('Enter the current time (HH-MM): ');
+   readln(currentTime);
+   write('Enter the file name (with path) for the left image: ');
+   readln(leftImageLocation);
+   write('Enter the file name (with path) for the right image: ');
+   readln(rightImageLocation);
+   writeln;
+   writeln('<Press ENTER to initiate program>');
+   readln;
+end; { userInput }
+
+(*
  *  Procedure to generate a product image
  *  from the left and right images
  *)
-procedure generateproductimage;
+procedure generateProductImage;
 begin
-   new(leftimage, rightimage ^.maxplane, rightimage ^.maxrow, rightimage ^.maxcol);
-   new(productimage, rightimage ^.maxplane, rightimage ^.maxrow, rightimage ^.maxcol);
-   
-   {productimage^ := rightimage^;}
-   productimage^ := leftimage^ * rightimage^;
+   new(newTestInteger);
+   new(leftImageTemp, leftImage ^.maxplane, leftImage ^.maxrow, leftImage ^.maxcol);
+   new(rightImageTemp, rightImage ^.maxplane, rightImage ^.maxrow, rightImage ^.maxcol);
+
+   writeln('In ''generate product image''');
+   {productImage^ := leftImage^ * rightImage^;}
 end; { generateproductimage }
 
 
 (*---------- Main body of program ----------*)
 begin
-   writeln('// First step for masters algorithm');
-   writeln('// Reads in two images and applies a correlation');
-   writeln;
-   write('Enter the file name (with path) for the left image: ');
-   readln(leftimagein);
-   write('Enter the file name (with path) for the right image: ');
-   readln(rightimagein);
-   
-   if loadimages(leftimagein, rightimagein) then
+   {initial user input}
+   userInput;
+
+   {if both images are read in succesfully then...}
+   if loadImages(leftImageLocation, rightImageLocation) then
    begin
-      generateproductimage;
-      storebmpfile('productimage.bmp', productimage^);
+      {generate a product image from the two input images}
+      generateProductImage;
       
+      {store the product image as a bmp file}
+      productImageLocation := currentDate + '_(' + currentTime + ')_ProductImage.bmp';
+      storebmpfile(productImageLocation, productImage^);
+
+      {indicate success}
       writeln;
       writeln('Success!');
    end
+   {if either/both of the images fail to be read in...}
    else
    begin
+      {indicate failure}
       writeln;
       writeln('Failure!');
    end;
+
+   dispose(leftImage);
+   dispose(rightImage);
+   dispose(productImage);
+   dispose(leftImageTemp);
+   dispose(rightImageTemp);
 end.
 (*---------- End of program ----------*)
